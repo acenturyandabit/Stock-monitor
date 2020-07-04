456aa060f8258e2ffb35eaf07d8f469832762d57
acenturyandabit <steeven.liu2@gmail.com>
2020-07-04 14:28:02 +1000

    auto pre-push commit

--- build.js
+++ build.js
@@ -44,7 +44,9 @@ function getAllUnder(base) {
     // drag the files from transfer to outside
     let transfers = getAllUnder('transfer');
     for (let i of transfers) {
-        fs.mkdirSync(i.slice(0, i.lastIndexOf("/")).slice("transfer/".length), { recursive: true });
+        if (i.lastIndexOf("/")>0){
+            fs.mkdirSync(i.slice(0, i.lastIndexOf("/")).slice("transfer/".length), { recursive: true });
+        }
         fs.renameSync(i, i.slice("transfer/".length));
     }
     execSync('git add .');
